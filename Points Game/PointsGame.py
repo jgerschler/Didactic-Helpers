f1c2438ab9603263810ed293ac53f3e7118076c6
jgerschler <jjgerschler@gmail.com>
2017-01-14 16:38:02 -0600

    updated

--- Points Game/PointsGame.py
+++ Points Game/PointsGame.py
@@ -3,45 +3,42 @@ from random import randint, sample
 from pygame.locals import *
 
 class PointsGame(object):
+    BLACK = (0,0,0)
+    WHITE = (255,255,255)
+    RED=(255,0,0)
+    
     def __init__(self):
         pygame.init()
         pygame.mixer.init()
 
-        self.DISPLAY_WIDTH = 1024# add to set_mode if a desired resolution is required.
-        self.DISPLAY_HEIGHT = 768
-
-        self.BLACK = (0,0,0)
-        self.WHITE = (255,255,255)
-        self.RED=(255,0,0)
-
         self.finished = False
 
-        self.SoundLoss0 = pygame.mixer.Sound('audio\\Laugh0.ogg')
-        self.SoundLoss1 = pygame.mixer.Sound('audio\\Laugh1.ogg')
-        self.SoundLoss2 = pygame.mixer.Sound('audio\\Laugh2.ogg')
-        self.SoundLoss3 = pygame.mixer.Sound('audio\\Laugh3.ogg')
+        self.sound_loss_0 = pygame.mixer.Sound('audio\\Laugh0.ogg')
+        self.sound_loss_1 = pygame.mixer.Sound('audio\\Laugh1.ogg')
+        self.sound_loss_2 = pygame.mixer.Sound('audio\\Laugh2.ogg')
+        self.sound_loss_3 = pygame.mixer.Sound('audio\\Laugh3.ogg')
 
-        self.SoundLossFiles = [self.SoundLoss0,self.SoundLoss1,self.SoundLoss2,self.SoundLoss3]
+        self.sound_loss_files = [self.sound_loss_0,self.sound_loss_1,self.sound_loss_2,self.sound_loss_3]
 
-        self.PointsList = ["10 points","20 points","30 points","40 points","-50 points","LOSE TURN"]
+        self.points_list = ["10 points","20 points","30 points","40 points","-50 points","LOSE TURN"]
 
-        self.GameDisplay = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+        self.game_display = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
         pygame.display.set_caption("Random Points Game")
-        self.GameDisplay.fill(self.WHITE)
+        self.game_display.fill(PointsGame.WHITE)
         pygame.display.update()
 
-        self.ActualDisplayWidth = self.GameDisplay.get_width()
-        self.ActualDisplayHeight = self.GameDisplay.get_height()
+        self.actual_display_width = self.game_display.get_width()
+        self.actual_display_height = self.game_display.get_height()
 
     def text_objects(self, text, font):
-        self.textSurface = font.render(text, True, self.BLACK)
-        return self.textSurface, self.textSurface.get_rect()
+        self.text_surface = font.render(text, True, PointsGame.BLACK)
+        return self.text_surface, self.text_surface.get_rect()
 
     def message_display(self, text):
-        self.largeText = pygame.font.Font('arial.ttf',180)
-        self.TextSurf, self.TextRect = self.text_objects(text, self.largeText)
-        self.TextRect.center = ((self.ActualDisplayWidth/2),(self.ActualDisplayHeight/2))
-        self.GameDisplay.blit(self.TextSurf, self.TextRect)
+        self.large_text = pygame.font.Font('arial.ttf',180)
+        self.text_surf, self.text_rect = self.text_objects(text, self.large_text)
+        self.text_rect.center = ((self.actual_display_width/2),(self.actual_display_height/2))
+        self.game_display.blit(self.text_surf, self.text_rect)
 
     def run(self):
         while not self.finished:
@@ -52,12 +49,12 @@ class PointsGame(object):
                     if event.key == pygame.K_SPACE:
                         listindex = randint(0,5)
                         if listindex in (4,5):
-                            self.GameDisplay.fill(self.RED)
-                            self.message_display(self.PointsList[listindex])
-                            sample(self.SoundLossFiles,1)[0].play()
+                            self.game_display.fill(PointsGame.RED)
+                            self.message_display(self.points_list[listindex])
+                            sample(self.sound_loss_files,1)[0].play()
                         else:
-                            self.GameDisplay.fill(self.WHITE)
-                            self.message_display(self.PointsList[listindex])
+                            self.game_display.fill(PointsGame.WHITE)
+                            self.message_display(self.points_list[listindex])
                             winsound.Beep(2000,500)
                         pygame.display.update()
                         
