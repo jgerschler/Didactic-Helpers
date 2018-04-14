e6f306980c79e1c8ec2601676eb069ba2d648b74
Jared Gerschler <jjgerschler@gmail.com>
2018-04-14 17:19:58 -0500

    2-team buzz in checkpoint

--- Team Buzz In/2-Team Buzz In with Audio/TeamSelectionWithAudio.py
+++ Team Buzz In/2-Team Buzz In with Audio/TeamSelectionWithAudio.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 import pygame
-import sys
 
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-
-
+finished = False
         
 pygame.init()
 pygame.mixer.init()
@@ -17,40 +15,55 @@ team_2_sound = pygame.mixer.Sound('audio\\equipo2.ogg')
 team_key_dict = {"Team1":[pygame.K_a,pygame.K_b,pygame.K_c,pygame.K_d],"Team2":[pygame.K_e,pygame.K_f,pygame.K_g,pygame.K_h]}
 
 clock = pygame.time.Clock()
-finished = False
 
 screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+screen_width = screen.get_width()
+screen_height = screen.get_width()
+
+font = pygame.font.Font(None, 128)
+
 screen.fill(WHITE)
 pygame.display.update()
 
-def wait(self):
-    while True:
+
+def wait(finished):
+    while not finished:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
             if event.type == pygame.KEYUP and event.key == pygame.K_SPACE:
                 return
-
+            if event.type == pygame.QUIT:
+                finished = True
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                finished = True
+    return finished
+                
 while not finished:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            self.finished = True
         if event.type == pygame.KEYUP:
-            if event.key in self.team_key_dict["Team1"]:
-                self.game_display.fill(TeamBuzzIn.WHITE)
-                self.message_display("Team 1",TeamBuzzIn.RED)
+            if event.key in team_key_dict["Team1"]:
+                screen.fill(WHITE)
+                team_text = font.render("1", 1, (255, 0, 0))
+                team_text_rect = team_text.get_rect()
+                team_text_rect.topright = (screen_width, 0)
+                screen.blit(team_text, team_text_rect)
                 pygame.display.update()
-                pygame.mixer.music.play()
-                self.wait()
-            elif event.key in self.team_key_dict["Team2"]:
-                self.game_display.fill(TeamBuzzIn.WHITE)
-                self.message_display("Team 2",TeamBuzzIn.YELLOW)
+                team_1_sound.play()
+                finished = wait(finished)
+            if event.key in team_key_dict["Team2"]:
+                screen.fill(WHITE)
+                team_text = font.render("2", 1, (255, 0, 0))
+                team_text_rect = team_text.get_rect()
+                team_text_rect.topright = (screen_width, 0)
+                screen.blit(team_text, team_text_rect)
                 pygame.display.update()
-                pygame.mixer.music.play()
-                self.wait()
+                team_2_sound.play()
+                finished = wait(finished)
+        if event.type == pygame.QUIT:
+            finished = True
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            finished = True
 
-    self.clock.tick(60)
+    clock.tick(60)
                 
 pygame.quit()
 
