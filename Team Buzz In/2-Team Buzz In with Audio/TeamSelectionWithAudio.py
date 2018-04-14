9e88b79ffb384d925df4dd783e496666d31dae2a
Jared Gerschler <jjgerschler@gmail.com>
2018-04-14 17:51:04 -0500

    need to test latest buzz in program at dif resolutions

--- Team Buzz In/2-Team Buzz In with Audio/TeamSelectionWithAudio.py
+++ Team Buzz In/2-Team Buzz In with Audio/TeamSelectionWithAudio.py
@@ -12,15 +12,32 @@ pygame.mixer.init()
 team_1_sound = pygame.mixer.Sound('audio\\equipo1.ogg')
 team_2_sound = pygame.mixer.Sound('audio\\equipo2.ogg')
 
+left_arrow = pygame.image.load('images\\left_arrow.png')
+right_arrow = pygame.image.load('images\\right_arrow.png')
+
 team_key_dict = {"Team1":[pygame.K_a,pygame.K_b,pygame.K_c,pygame.K_d],"Team2":[pygame.K_e,pygame.K_f,pygame.K_g,pygame.K_h]}
 
 clock = pygame.time.Clock()
 
 screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
 screen_width = screen.get_width()
-screen_height = screen.get_width()
+screen_height = screen.get_height()
 
-font = pygame.font.Font(None, 128)
+font = pygame.font.Font(None, 512)
+
+team_text_1 = font.render("1", 1, (255, 0, 0))
+team_text_rect_1 = team_text_1.get_rect()
+team_text_rect_1.center = (screen_width / 2, screen_height / 2)
+
+team_text_2 = font.render("2", 1, (255, 0, 0))
+team_text_rect_2 = team_text_2.get_rect()
+team_text_rect_2.center = (screen_width / 2, screen_height / 2)
+
+left_arrow_rect = left_arrow.get_rect()
+right_arrow_rect = right_arrow.get_rect()
+
+left_arrow_rect.midleft = (100, screen_height / 2)
+right_arrow_rect.midright = (screen_width - 100, screen_height / 2)
 
 screen.fill(WHITE)
 pygame.display.update()
@@ -42,19 +59,15 @@ while not finished:
         if event.type == pygame.KEYUP:
             if event.key in team_key_dict["Team1"]:
                 screen.fill(WHITE)
-                team_text = font.render("1", 1, (255, 0, 0))
-                team_text_rect = team_text.get_rect()
-                team_text_rect.topright = (screen_width, 0)
-                screen.blit(team_text, team_text_rect)
+                screen.blit(team_text_1, team_text_rect_1)
+                screen.blit(left_arrow, left_arrow_rect)
                 pygame.display.update()
                 team_1_sound.play()
                 finished = wait(finished)
             if event.key in team_key_dict["Team2"]:
                 screen.fill(WHITE)
-                team_text = font.render("2", 1, (255, 0, 0))
-                team_text_rect = team_text.get_rect()
-                team_text_rect.topright = (screen_width, 0)
-                screen.blit(team_text, team_text_rect)
+                screen.blit(team_text_2, team_text_rect_2)
+                screen.blit(right_arrow, right_arrow_rect)
                 pygame.display.update()
                 team_2_sound.play()
                 finished = wait(finished)
